4f43e584e70c2df957105a2e5664e03dee9b834a
Michael Zaleski <me@mikezaleski.com>
2020-04-15 06:20:45 -0400

    basic components and styles working

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
-import React from "react";
-import "./App.css";
+import React from 'react';
 
-import HomePage from "./homepage.component.jsx";
-import './homepage.styles.scss';
+import HomePage from './pages/homepage/homepage.component';
 
-function App() {
-  return <div>
-    <HomePage/>
-  </div>;
+import './App.css';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className='App'>
+        <HomePage />
+      </div>
+    );
+  }
 }
 
 export default App;
