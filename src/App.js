cfe6a4cc7d48d20fe4dd672c510e4d820f1efce7
Michael Zaleski <me@mikezaleski.com>
2020-04-18 12:18:07 -0400

    added Google user auth subscription

--- src/App.js
+++ src/App.js
@@ -7,8 +7,31 @@ import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from './pages/shop/shop.component';
 import Header from "./components/header/header.component";
 import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
+import { auth } from './firebase/firebase.utils';
 
 class App extends React.Component {
+  constructor(props) {
+    super (props);
+
+    this.state = {
+      currentUser: null
+    }
+  }
+
+  unsubscribeFromAuth = null
+
+  componentDidMount() {
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
+      this.setState({ currentUser: user });
+
+      console.log(user);
+    });
+  }
+  
+  componentWillUnmount() {
+    this.unsubscribeFromAuth();
+  }
+
   render() {
     return (
       <div>
