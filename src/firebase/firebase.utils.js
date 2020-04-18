1e3a32df7a7e5f7a9e0a47f5161bde4aa339daeb
Michael Zaleski <me@mikezaleski.com>
2020-04-18 10:33:40 -0400

    added firebase, firebase.utils.js, and added signInWithGoogle functionality to sign-in.component

---
+++ src/firebase/firebase.utils.js
@@ -0,0 +1,25 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+const config = {
+    apiKey: "AIzaSyCiCngry7bVIlbH29o3zfp6LWMfa3h7ecY",
+    authDomain: "crwn-db-1817f.firebaseapp.com",
+    databaseURL: "https://crwn-db-1817f.firebaseio.com",
+    projectId: "crwn-db-1817f",
+    storageBucket: "crwn-db-1817f.appspot.com",
+    messagingSenderId: "816304759922",
+    appId: "1:816304759922:web:6b975eb1d0d97e84df8eeb",
+    measurementId: "G-P907HJR8SD"
+  };
+
+firebase.initializeApp(config);
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+
+const provider = new firebase.auth.GoogleAuthProvider();
+provider.setCustomParameters({ prompt: 'select_account' });
+export const signInWithGoogle = () => auth.signInWithPopup(provider);
+
+export default firebase;
